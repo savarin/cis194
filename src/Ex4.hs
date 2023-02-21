447409e2490d28b44d2e8a45d2511be3a51fccae
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-02-21 13:16:04 -0800

    hw 4 - up to ex 2

--- src/Ex4.hs
+++ src/Ex4.hs
@@ -2,7 +2,10 @@
 
 module Ex4
     ( fun1,
-      fun2
+      fun2,
+      seed,
+      grow,
+      foldTree
     ) where
 
 -- Exercise 1: Wholemeal programming
@@ -31,3 +34,54 @@ fun1 = foldl (\acc x -> acc * (x - 2)) 1 . filter even
 
 fun2 :: Integer -> Integer
 fun2 = sum . filter even .takeWhile (>1) . iterate (\n -> if even n then div n 2 else 3 * n + 1)
+
+
+-- Exercise 2: Folding with trees
+-- Recall the definition of a binary tree data structure. The height of a binary
+-- tree is the length of a path from the root to the deepest node. For example,
+-- the height of a tree with a single node is 0; the height of a tree with three
+-- nodes, whose root has two children, is 1; and so on. A binary tree is
+-- balanced if the height of its left and right subtrees differ by no more than
+-- 1, and its left and right subtrees are also balanced.
+
+-- You should use the following data structure to represent binary trees. Note
+-- that each node stores an extra Integer representing the height at that node.
+
+--   data Tree a = Leaf
+--               | Node Integer (Tree a) a (Tree a)
+--     deriving (Show, Eq)
+
+-- For this exercise, write a function
+--   foldTree :: [a] -> Tree a
+--   foldTree = ...
+-- which generates a balanced binary tree from a list of values using foldr.
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+seed :: Tree a
+seed = Leaf
+
+countNodes :: Tree a -> Integer
+countNodes Leaf = 0
+countNodes (Node _ left x right) = (countNodes left) + 1 + (countNodes right)
+
+isBalanced :: Tree a -> Bool
+isBalanced Leaf = True
+isBalanced tree@(Node n _ _ _) = countNodes tree == 2^(n + 1) - 1
+
+addHeight :: Integer -> Tree a -> Integer
+addHeight n tree = if isBalanced tree then n + 1 else n
+
+grow :: a -> Tree a -> Tree a
+grow x Leaf = Node 0 Leaf x Leaf
+grow x (Node n left y right)
+  | countLeft > countRight = Node n left y (grow x right)
+  | countLeft < countRight = Node n (grow x left) y right
+  | otherwise              = Node (addHeight n left) (grow x left) y right
+  where countLeft  = countNodes left
+        countRight = countNodes right
+
+foldTree :: [a] -> Tree a
+foldTree = foldr grow seed
