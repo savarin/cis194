235ab71258f803ed9739d23879de59e3267f51fe
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-02-21 15:54:44 -0800

    hw 5 - up to ex 2

--- src/Ex5.hs
+++ src/Ex5.hs
@@ -2,14 +2,11 @@
 
 module Ex5
     ( ExprT (Lit, Add, Mul),
-      eval
+      eval,
+      evalStr
     ) where
 
-
-data ExprT  = Lit Integer
-            | Add ExprT ExprT
-            | Mul ExprT ExprT
-  deriving (Show, Eq)
+import Parser
 
 
 -- Exercise 1
@@ -18,7 +15,39 @@ data ExprT  = Lit Integer
 
 -- For example, eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20.
 
+data ExprT  = Lit Integer
+            | Add ExprT ExprT
+            | Mul ExprT ExprT
+  deriving (Show, Eq)
+
 eval :: ExprT -> Integer
 eval (Lit n) = n
 eval (Add (Lit n) (Lit m)) = n + m
 eval (Mul (Lit n) (Lit m)) = n * m
+
+
+-- Exercise 2
+-- The UI department has internalized the focus group data and is ready to
+-- synergize with you. They have developed the front-facing user-interface: a
+-- parser that handles the textual representation of the selected language. They
+-- have sent you the module Parser.hs, which exports parseExp, a parser for
+-- arithmetic expressions. If you pass the constructors of ExprT to it as
+-- arguments, it will convert Strings representing arithmetic expressions into
+-- values of type ExprT.
+
+-- For example:
+--   *Calc> parseExp Lit Add Mul "(2+3)*4"
+--   Just (Mul (Add (Lit 2) (Lit 3)) (Lit 4))
+--   *Calc> parseExp Lit Add Mul "2+3*4"
+--   Just (Add (Lit 2) (Mul (Lit 3) (Lit 4)))
+--   *Calc> parseExp Lit Add Mul "2+3*"
+--   Nothing
+
+-- Leverage the assets of the UI team to implement the value-added function
+--   evalStr :: String -> Maybe Integer
+-- which evaluates arithmetic expressions given as a String, producing Nothing
+-- for inputs which are not well-formed expressions, and Just n for well-formed
+-- inputs that evaluate to n.
+
+evalStr :: String -> Maybe Integer
+evalStr = (eval <$>) . parseExp Lit Add Mul
