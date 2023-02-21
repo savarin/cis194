f493f774bab8051eb3c7d80397c5996d8015efa0
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-02-21 06:48:39 -0800

    hw 3 - up to ex 2

--- src/Ex3.hs
+++ src/Ex3.hs
@@ -1,5 +1,6 @@
 module Ex3
-    ( skips
+    ( skips,
+      localMaxima
     ) where
 
 -- Exercise 1 Hopscotch
@@ -22,8 +23,32 @@ module Ex3
 
 skips' :: Int -> [a] -> [a]
 skips' n x
-  | n >= length x = []
-  | otherwise     = [z] ++ skips' n (tail y) where y@(z:_) = drop n x
+  | (z:zs) <- y     = [z] ++ skips' n zs
+  | [] <- y         = []
+  where y = drop n x
 
 skips :: [a] -> [[a]]
 skips x = [skips' n x | n <- [0 .. (length x - 1)]]
+
+
+-- Exercise 2 Local maxima
+-- A local maximum of a list is an element of the list which is strictly greater
+-- than both the elements immediately before and after it. For example, in the
+-- list [2,3,4,1,5], the only local maximum is 4, since it is greater than the
+-- elements immediately before and after it (3 and 1). 5 is not a local maximum
+-- since there is no element that comes after it.
+
+-- Write a function
+--   localMaxima :: [Integer] -> [Integer]
+-- which finds all the local maxima in the input list and returns them in order.
+
+-- For example:
+--   localMaxima [2,9,5,6,1] == [9,6]
+--   localMaxima [2,3,4,1,5] == [4]
+--   localMaxima [1,2,3,4,5] == []
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:zs)
+  | x < y && y > z = [y] ++ localMaxima (z:zs)
+  | otherwise      = [] ++ localMaxima (y:z:zs)
+localMaxima _ = []
