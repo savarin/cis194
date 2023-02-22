c45b712920406018d3dfa807fa9d0deaa6d19698
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-02-21 17:27:21 -0800

    hw 6 - up to ex 2

--- src/Ex6.hs
+++ src/Ex6.hs
@@ -23,3 +23,15 @@ fib n = fib (n - 1) + fib (n - 2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
+
+
+-- Exercise 2
+-- Your task for this exercise is to come up with more efficient implementation.
+-- Specifically, define the infinite list
+--   fibs2 :: [Integer]
+-- so that it has the same elements as fibs1, but computing the first n elements
+-- of fibs2 requires only O(n) addition operations. Be sure to use standard
+-- recursion pattern(s) from the Prelude as appropriate.
+
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : (zipWith (+) fibs2 (tail fibs2))
